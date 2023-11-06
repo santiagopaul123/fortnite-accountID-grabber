53c3dc01188fa9756f0ba0f0d02aba5a9c155a2b
santiagopaul123 <92693747+santiagopaul123@users.noreply.github.com>
2023-11-06 14:33:10 -0800

    Update Grabber.py

--- Grabber.py
+++ Grabber.py
@@ -1,17 +1,16 @@
-######                 #####   #####   #####    ###   
-#     # ###### #    # #     # #     # #     #  #   #  
-#     # #      #    # #       #     #       # #     # 
-#     # #####  #    # ######   ######  #####  #     # 
-#     # #      #    # #     #       # #       #     # 
-#     # #       #  #  #     # #     # #        #   #  
-######  ######   ##    #####   #####  #######   ###   
-                                                      
+######                 
+#     # ###### #    #  
+#     # #      #    # 
+#     # #####  #    # 
+#     # #      #    # 
+#     # #       #  #   
+######  ######   ##     
+
+
 import requests
 import re
-import ctypes
-ctypes.windll.kernel32.SetConsoleTitleW("Coded by github.com/dev6920")
+import ctypes  
 
-print("Coded by dev github/dev6920")
 
 while True:
   
